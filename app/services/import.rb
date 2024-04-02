54912b048fdf3df863e6533811d11b51eaf91291
stap780 <panaet80@mail.ru>
2024-04-02 12:55:51 +0300

    change image create

--- app/services/import.rb
+++ app/services/import.rb
@@ -84,29 +84,29 @@ class Services::Import
         7 => {"-brightness-contrast" => "10x5"},
         8 => {"-unsharp" => "95%"},
         9 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-crop" => "900x900+50+200"},
-        10 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-crop" => "999x999+100+100"},
-        11 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-crop" => "999x999+120+120"},
-        12 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-crop" => "980x980+130+130"},
-        13 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-brightness-contrast" => "10x5"},
-        14 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-unsharp" => "95%"},
-        15 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-crop" => "900x900+50+200"},
-        16 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-crop" => "999x999+100+100"},
-        17 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-crop" => "999x999+120+120"},
-        18 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-crop" => "980x980+130+130"},
-        19 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-brightness-contrast" => "10x5"},
-        20 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-unsharp" => "95%"},
-        21 => {"-crop" => "900x900+50+200","-brightness-contrast" => "10x5"},
-        22 => {"-crop" => "900x900+50+200","-unsharp" => "95%"},
-        23 => {"-crop" => "999x999+100+100","-brightness-contrast" => "10x5"},
-        24 => {"-crop" => "999x999+100+100","-unsharp" => "95%"},
-        25 => {"-crop" => "999x999+120+120","-brightness-contrast" => "10x5"},
-        26 => {"-crop" => "999x999+120+120","-unsharp" => "95%"},
-        27 => {"-crop" => "980x980+130+130","-brightness-contrast" => "10x5"},
-        28 => {"-crop" => "980x980+130+130","-unsharp" => "95%"},
-        29 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-crop" => "980x980+130+130","-unsharp" => "95%"}
+        10 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-crop" => "999x999+100+100"}
+        # 11 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-crop" => "999x999+120+120"},
+        # 12 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-crop" => "980x980+130+130"},
+        # 13 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-brightness-contrast" => "10x5"},
+        # 14 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-unsharp" => "95%"},
+        # 15 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-crop" => "900x900+50+200"},
+        # 16 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-crop" => "999x999+100+100"},
+        # 17 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-crop" => "999x999+120+120"},
+        # 18 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-crop" => "980x980+130+130"},
+        # 19 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-brightness-contrast" => "10x5"},
+        # 20 => {"-rotate" => "-2", "-background" => "transparent", "-quality" => "92","-unsharp" => "95%"},
+        # 21 => {"-crop" => "900x900+50+200","-brightness-contrast" => "10x5"},
+        # 22 => {"-crop" => "900x900+50+200","-unsharp" => "95%"},
+        # 23 => {"-crop" => "999x999+100+100","-brightness-contrast" => "10x5"},
+        # 24 => {"-crop" => "999x999+100+100","-unsharp" => "95%"},
+        # 25 => {"-crop" => "999x999+120+120","-brightness-contrast" => "10x5"},
+        # 26 => {"-crop" => "999x999+120+120","-unsharp" => "95%"},
+        # 27 => {"-crop" => "980x980+130+130","-brightness-contrast" => "10x5"},
+        # 28 => {"-crop" => "980x980+130+130","-unsharp" => "95%"},
+        # 29 => {"-rotate" => "2", "-background" => "transparent", "-quality" => "92","-crop" => "980x980+130+130","-unsharp" => "95%"}
     }
 
-    var_count = 29
+    var_count = 10 #29
 
     if product.variants.size < var_count
         #create variants with images
