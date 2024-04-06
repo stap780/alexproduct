0567825f7c6ec1bb816fe2913cb96f896766c76a
stap780 <panaet80@mail.ru>
2024-04-06 11:22:32 +0300

    change cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,11 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+set :sidekiq_roles => :worker
+set :sidekiq_default_hooks => true
+set :sidekiq_env => fetch(:rack_env, fetch(:rails_env, fetch(:stage)))
+# single config
+set :sidekiq_config_files, ['sidekiq.yml']
 
 append :linked_files, "config/master.key", "config/database.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "public", 'tmp/sockets', 'vendor/bundle', 'lib/tasks', 'lib/drop', 'storage'
